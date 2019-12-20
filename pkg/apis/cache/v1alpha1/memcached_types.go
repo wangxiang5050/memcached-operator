38dadedf2c7470d6f7a2884915d33c1400a8b031
wangxiang5050 <wangxiang5050@aliyun.com>
2019-12-20 15:37:38 +0800

    change CR struct

--- pkg/apis/cache/v1alpha1/memcached_types.go
+++ pkg/apis/cache/v1alpha1/memcached_types.go
@@ -12,6 +12,7 @@ type MemcachedSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	name string `json:"name"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
@@ -19,6 +20,7 @@ type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	validated string `json: validated`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
