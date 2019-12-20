e6017f5eef3a5a6b5e3e20670b153ec1e9c2d100
wangxiang5050 <wangxiang5050@aliyun.com>
2019-12-20 15:40:23 +0800

    change CR struct

--- pkg/apis/cache/v1alpha1/memcached_types.go
+++ pkg/apis/cache/v1alpha1/memcached_types.go
@@ -20,7 +20,7 @@ type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	validated string `json: validated`
+	validated string `json:validated`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
